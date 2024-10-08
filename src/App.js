3ebc85428d671970651b8661e1d2d1fd31fb1865
soumya1103 <soumya.agrawal@nucleusteq.com>
2024-10-08 12:38:02 +0530

    Forgot password page created

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import "./App.css";
+import ForgotPassword from "./Pages/ForgotPassword/ForgotPassword";
 import Login from "./Pages/Login/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Login />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/forgotPassword" element={<ForgotPassword />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
