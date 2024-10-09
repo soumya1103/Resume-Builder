70aa12bcc4fcc455ee2c2c6569c8d75f7e865105
soumya1103 <soumya.agrawal@nucleusteq.com>
2024-10-09 18:43:38 +0530

    Pulled login branch

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Login from "./Pages/Login/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import PersonalInfo from "./Pages/CreateResume/PersonalInfo";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Education from "./Pages/CreateResume/Education";
 import ProfessionalExperience from "./Pages/CreateResume/ProfessionalExperience";
 import Skills from "./Pages/CreateResume/Skills";
@@ -16,29 +15,11 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
           <Route path="/personalInfo" element={<PersonalInfo />} />
-        </Routes>
-        <Routes>
           <Route path="/education" element={<Education />} />
-        </Routes>
-        <Routes>
           <Route path="/professionalExperience" element={<ProfessionalExperience />} />
-        </Routes>
-        <Routes>
           <Route path="/skills" element={<Skills />} />
-        </Routes>
-        <Routes>
           <Route path="/awardCerts" element={<AwardCertification />} />
-        </Routes>
-        <Routes>
           <Route path="/professionalSummary" element={<ProfessionalSummary />} />
         </Routes>
       </BrowserRouter>
