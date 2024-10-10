c52cf542ea9e35ce31abfae2dbf65554e7b28147
tanu-rathore02 <tanu.rtr14@gmail.com>
2024-10-10 17:47:44 +0530

    dashboard design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import "./App.css";
+import Dashboard from "./Pages/Dashboard/Dashboard";
 import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,10 +11,13 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Login />} />
+                    <Route path="/register" element={<Register/>}/>
+                    <Route path= "/dashboard" element= {<Dashboard/>}/>
                 </Routes>
             </BrowserRouter>
         </div>
     );
+
 }
 
 export default App;
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import "../Register/Register.css";
+import { TextField } from "@mui/material";
+import Button from "../../Components/Button/Button";
+
 
 function Register() {
+  console.log("register page");
   return (
-    <div>Register</div>
-  )
+    <div className="register-container-outer">
+      <div className="register-container-inner">
+        <form className="register-form">
+          {/* <img src={logo} alt="logo" width="50%" /> */}
+          <h1>Register</h1>
+          <TextField label="Username" variant="outlined" margin="dense" className="register-input" />
+          <br />
+          <TextField label="Email" variant="outlined" margin="dense" className="register-input" />
+          <br />
+          <TextField label="Password" type="password" variant="outlined" margin="normal" className="register-input" />
+          <br />
+          {/* <TextField label="Role" variant="standard" margin="dense"/> */}
+          <br />
+          <Button className="register-btn">Register</Button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
