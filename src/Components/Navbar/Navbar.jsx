89f388c068be6f717ae53c1eac65ab0676cc677d
tanu-rathore02 <tanu.rtr14@gmail.com>
2024-10-11 13:36:58 +0530

    navbar updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,12 @@
+
+
 import React, { useState } from 'react';
 import '../Navbar/Navbar.css';
 import NT from '../../Images/NucleusTeq Logo.png';
+import { logout } from "../../Api/apiService";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "../../Redux/Authentication/AuthenticationAction";
 
 function Navbar() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -13,10 +19,21 @@ function Navbar() {
     setIsDropdownOpen(false);
   };
 
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    dispatch(logoutUser());
+    navigate("/");
+  };
+
+
   return (
-    <div className='navbar'>
-      <div className='navbar-logo'>
-        <img src={NT} alt='Logo' />
+    <div className="navbar">
+      <div className="navbar-logo">
+        <img src={NT} alt="Logo" />
       </div>
       <h2 className='heading'>Welcome User</h2>
       <div 
@@ -25,9 +42,11 @@ function Navbar() {
       >
         <button className='btn' onMouseEnter={toggleDropdown}>T</button>
         {isDropdownOpen && (
-          <div className='dropdown-menu'>
-            <button className='dropdown-item'>Profile</button>
-            <button className='dropdown-item'>Logout</button>
+          <div className="dropdown-menu">
+            <button className="dropdown-item">Profile</button>
+            <button className="dropdown-item" onClick={handleLogout}>
+              Logout
+            </button>
           </div>
         )}
       </div>
