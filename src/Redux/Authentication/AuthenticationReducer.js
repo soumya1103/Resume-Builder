b7976c3e16436e48ec3e6f85161bfc3246018768
soumya1103 <soumya.agrawal@nucleusteq.com>
2024-10-14 16:05:43 +0530

    Reducer changed

--- src/Redux/Authentication/AuthenticationReducer.js
+++ src/Redux/Authentication/AuthenticationReducer.js
@@ -1,20 +1,35 @@
 import { LOGIN, LOGOUT } from "./AuthenticationTypes";
 
-const initialState = {
-  name: "",
-  email: "",
-  role: "",
-  token: "",
-  userId: "",
-};
+// Retrieve any existing auth data from localStorage
+const persistedState = localStorage.getItem("auth")
+  ? JSON.parse(localStorage.getItem("auth"))
+  : {
+      name: "",
+      email: "",
+      role: "",
+      token: "",
+      userId: "",
+    };
+
+const initialState = persistedState;
 
 const authReducer = (state = initialState, action) => {
   switch (action.type) {
     case LOGIN:
+      // Store the user data in localStorage when the user logs in
+      localStorage.setItem("auth", JSON.stringify(action.payload));
       return action.payload;
 
     case LOGOUT:
-      return initialState;
+      // Remove user data from localStorage when the user logs out
+      localStorage.removeItem("auth");
+      return {
+        name: "",
+        email: "",
+        role: "",
+        token: "",
+        userId: "",
+      };
 
     default:
       return state;
