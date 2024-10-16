d8d07753d285862eefeef7307b05a2baa1395f9f
soumya1103 <soumya.agrawal@nucleusteq.com>
2024-10-16 12:58:12 +0530

    Profile page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Login from "./Pages/Login/Login";
 import { Routes, Route } from "react-router-dom";
+import Profile from "./Pages/Profile/Profile";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   );
---
+++ src/Pages/Profile/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Profile.css";
+
+function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-picture-container"></div>
+    </div>
+  );
+}
+
+export default Profile;
