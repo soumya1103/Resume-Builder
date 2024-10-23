12e22fb19d9c30627549e510305c540b53d445c7
soumya1103 <soumya.agrawal@nucleusteq.com>
2024-10-23 17:41:47 +0530

    Create resume updated with resume title

--- src/Api/apiService.js
+++ src/Api/apiService.js
@@ -1,14 +1,39 @@
 import app from "./apiClient";
-import { CREATE_USER, GET_USER_BY_ID, LOGIN } from "./apiConstants";
+import { CREATE_USER, GET_USER_BY_ID, LOGIN, RESET_PASSWORD, RESUME_TITLE, SEND_OTP, UPDATE_PROFILE } from "./apiConstants";
+import { VIEW_RESUME } from "./apiConstants";
 
 export const login = async (email, password) => {
   return await app.post(LOGIN, { email, password });
+};
+
+export const logout = () => {
+  window.localStorage.removeItem("authtoken");
 };
 
 export const getUserById = async (userId) => {
   return await app.get(`${GET_USER_BY_ID}/${userId}`);
 };
 
-export const addUser = async (userData) => {
-  return await app.post(CREATE_USER, userData);
+export const addUser = async (profileId, userData) => {
+  return await app.put(`${CREATE_USER}/${profileId}`, userData);
+};
+
+export const view_resume = async (userId) => {
+  return await app.get(VIEW_RESUME(userId));
+};
+
+export const updateProfile = async (userId, data) => {
+  return await app.put(`${UPDATE_PROFILE}/${userId}`, data);
+};
+
+export const sendOtp = async (email) => {
+  return await app.post(`${SEND_OTP}?email=${email}`);
+};
+
+export const resetPassword = async (email, otp, password) => {
+  return await app.post(`${RESET_PASSWORD}?email=${email}&otp=${otp}&newPassword=${password}`);
+};
+
+export const saveResumeTitle = async (id) => {
+  return await app.post(RESUME_TITLE, { id });
 };
