33fc845978e20a30868c83a4400eeedf30b8f3f9
avantigupta <avanti.gupta@nucleusteq.com>
2024-11-25 15:23:55 +0530

    submit button changed

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -3,58 +3,13 @@ import { Link, useLocation } from "react-router-dom";
 import "./Sidebar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleInfo, faUserGraduate, faUserTie, faWindowRestore, faList, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-import Button from "../Button/Button";
-import { addCandidate, addUser } from "../../Api/apiService";
 import { ToastContainer, toast } from "react-toastify";
 import { useSelector } from "react-redux";
 
 function Sidebar() {
   const location = useLocation();
 
-  const userData = useSelector((state) => state.resume);
-
   const role = useSelector((state) => state.auth);
-
-  const selectedRole = localStorage.getItem("selectedRole") || { selectedRole: "" };
-
-  const profileId = localStorage.getItem("profileId"); 
-  
-  const handleSubmit = async () => {
-    if (selectedRole === "candidate") {
-      try {
-        const response = await addCandidate(profileId, userData);
-        if (response.status === 200 || response.status === 201) {
-          toast.success(response?.data?.message, {
-            autoClose: 2000,
-          });
-        }
-        setTimeout(() => {
-          window.location.href = role.role === "ROLE_HR" ? "dashboardHr" : "/dashboard";
-        }, 3000);
-      } catch (error) {
-        toast.error(error?.response?.data?.message || "Something went wrong.", {
-          autoClose: 2000,
-        });
-      }
-    } else {
-      try {
-        const response = await addUser(profileId, userData);
-        console.log(response)
-        if (response.status === 200 || response.status === 201) {
-          toast.success(response?.data?.message, {
-            autoClose: 2000,
-          });
-        }
-        setTimeout(() => {
-          window.location.href = role.role === "ROLE_HR" ? "dashboardHr" : "/dashboard";
-        }, 3000);
-      } catch (error) {
-        toast.error(error?.response?.data?.message || "Something went wrong.", {
-          autoClose: 2000,
-        });
-      }
-    }
-  };
 
   return (
     <div className="sidebar-container">
@@ -96,9 +51,6 @@ function Sidebar() {
             </button>
           </div>
         </div>
-        <Button className="sidebar-submit-btn" onClick={handleSubmit}>
-          <h4>Submit</h4>
-        </Button>
       </div>
       <ToastContainer />
     </div>
